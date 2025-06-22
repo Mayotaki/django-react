41ce9aae7f77b16b25d9db0045f03416d191fc7a
Evck <egorkavtaskin@25.local>
2025-06-22 13:28:36 +0300

    Initial commit

---
+++ ezrequests/printers.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.zackplauche.com'
+
+
+
+def h1_printer(url):
+    """Get the h1 tag from any url ad print it!"""
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    h1_tag = soup.find('h1')
+    print(h1_tag.text)
